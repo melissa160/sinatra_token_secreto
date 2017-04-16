c7b9c3619a5406749328b89e9783609ece1428d1
melissa160 <melissa160@gmail.com>
2017-04-16 13:02:03 -0500

    actualizando archivos para pruebas makeitreal

--- app/models/README.md
+++
@@ -1,5 +0,0 @@
-Modelos de active record van aca.
-
-Los puedes crear con el siguiente comando:
-
-`bundle exec rake generate:model NAME=User`
--- solution.rb
+++ solution.rb
@@ -1,5 +1,6 @@
+require 'sinatra'
+
 get "/" do
-  puts "*"*60
   if  request.env['HTTP_PERMISO'] == "soy-un-token-secreto"
     "Si lo logramos!"
   else
